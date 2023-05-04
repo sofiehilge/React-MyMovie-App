727b56b5311dd034934a624691cc9ff1cf3f9c79
sofiehilge <sofie.hilge@gmail.com>
2023-05-04 12:18:30 +0200

    add styling to errorelement

--- src/pages/ErrorView.jsx
+++ src/pages/ErrorView.jsx
@@ -1,5 +1,29 @@
+import Lottie from "lottie-react";
+import dinoAnimation from "../assets/lottiefiles/error.json";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import Heading from "../components/Heading";
+
+const StyledLottie = styled(Lottie)`
+  width: 100vw;
+  height: 100vw;
+`;
+
+const StyledLink = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 2rem;
+`;
+
 const ErrorView = () => {
-    return ( <>404</> );
-}
- 
+  return (
+    <StyledLink>
+      <Heading title="Sorry something went wrong" size="24" as="h2" />
+      <StyledLottie animationData={dinoAnimation} />;
+      <Heading title="Click here to return to frontpage" size="18" as="h2" />
+    </StyledLink>
+  );
+};
+
 export default ErrorView;
